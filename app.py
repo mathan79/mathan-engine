3976f026d3cb1edde42297a667bfb35c6e663b35
mathan79 <mathan_vijai@yahoo.com.sg>
2026-03-23 15:44:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
 from flask import Flask, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/")
 def home():
@@ -11,7 +9,7 @@ def home():
 
 @app.route("/ping")
 def ping():
-    return jsonify({"status": "OK", "system": "HEMAN"})
+    return jsonify({"ok": True})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
