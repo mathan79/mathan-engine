342dd0efbb5d4b1fff5a36751eb9de3d0cc68c1f
mathan79 <mathan_vijai@yahoo.com.sg>
2026-03-24 18:10:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "HEMAN LIVE"
+    return "MATHAN AI HEMAN - LIVE!"
+
+@app.route("/ping")
+def ping():
+    return jsonify(status="OK")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
